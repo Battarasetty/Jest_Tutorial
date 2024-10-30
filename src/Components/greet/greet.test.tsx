9a59c75c56cf6ed4bc4ffd0142790729c3da5756
Battarasetty <harish93601@gmail.com>
2024-10-30 13:12:11 +0530

    describe in just add

--- src/Components/greet/greet.test.tsx
+++ src/Components/greet/greet.test.tsx
@@ -1,14 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // describe('Nested', () => {
+  //     test("Greet renders a name", () => {
+  //         render(<Greet name="Harish" />);
+  //         const textElement = screen.getByText("Hello Harish");
+  //         expect(textElement).toBeInTheDocument();
+  //     });
+  // })
 });
 
-test("Greet renders with name", () => {
-  render(<Greet name="Harish" />);
-  const textElement = screen.getByText("Hello Harish");
-  expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+  test("Greet renders a name", () => {
+    render(<Greet name="Harish" />);
+    const textElement = screen.getByText("Hello Harish");
+    expect(textElement).toBeInTheDocument();
+  });
 });
