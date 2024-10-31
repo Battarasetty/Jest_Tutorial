60d4b8f70a54c2b7bef6829370e00d10e2760de8
Battarasetty <harish93601@gmail.com>
2024-10-31 12:19:01 +0530

    filename convetions add

---
+++ src/Components/greet/greet.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import Greet from "./greet";
+
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  // describe('Nested', () => {
+  //     test("Greet renders a name", () => {
+  //         render(<Greet name="Harish" />);
+  //         const textElement = screen.getByText("Hello Harish");
+  //         expect(textElement).toBeInTheDocument();
+  //     });
+  // })
+});
+
+describe("Nested", () => {
+  test("Greet renders a name", () => {
+    render(<Greet name="Harish" />);
+    const textElement = screen.getByText("Hello Harish");
+    expect(textElement).toBeInTheDocument();
+  });
+});
